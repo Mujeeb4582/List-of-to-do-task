d123b6c2df64fb85de41a4754b4e061fdea673a4
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-11-04 16:20:51 +0500

    update the file for clear functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './style.css';
 import populate from './module/populatetask.js';
 import { getAddedTodos, form } from './module/newTask.js';
 import deleteTask from './module/removeTask.js';
+import clearCompleted from './module/clearCompleted.js';
+
+const clearCompletedButton = document.getElementById('clearCompleted');
 
 populate();
 form.addEventListener('submit', getAddedTodos);
@@ -11,3 +14,5 @@ document.addEventListener('click', (e) => {
     deleteTask(e.target.id);
   }
 });
+
+clearCompletedButton.addEventListener('click', clearCompleted);
