577f994ee6daf86072b42f705ad0fa8b717d62ca
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-11-09 15:06:23 +0500

    Add the test for local storage

---
+++ unit test/localStorage.test.js
@@ -0,0 +1,26 @@
+import LocalStorage from './mocks/localStorage.js';
+
+describe('localStorage', () => {
+  it('Adds an item to the mock localStorage', () => {
+    const localStorage = new LocalStorage();
+    const key = 'mockKey';
+    const value = 'stringValue';
+
+    localStorage.setItem(key, value);
+
+    expect(
+      Object.keys(localStorage.storage).length,
+    ).toEqual(1);
+  });
+
+  it('gets the items from the mock localStorage', () => {
+    const localStorage = new LocalStorage();
+    const key = 'mockKey';
+    const value = 'stringValue';
+    localStorage.setItem(key, value);
+
+    const receivedValue = localStorage.getItem(key);
+
+    expect(receivedValue).toEqual(value);
+  });
+});
